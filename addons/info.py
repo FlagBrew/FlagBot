e28bd655b67f1da4efdce2b8ecc1f664b69b5b14
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-11-12 01:20:41 -0800

    Command touch ups

--- addons/info.py
+++ addons/info.py
@@ -8,13 +8,15 @@ class Info:
         print('Addon "{}" loaded'.format(self.__class__.__name__))
         
         
-    @commands.command()
+    @commands.command(aliases=['releases'])
     async def release(self, ctx):
+        """Gives the latest PKSM release"""
         embed = discord.Embed(description="You can get the latest release of PKSM [here](https://github.com/BernardoGiordano/PKSM/releases/latest)")
         await ctx.send(embed=embed)
         
     @commands.command()
     async def about(self, ctx):
+        """Information about the bot"""
         await ctx.send("This is a bot coded in python for use in the PKSM server, made by Griffin#2329. You can view the source code [here](https://github.com/GriffinG1/PSKMBot).")
         
 def setup(bot):
--- addons/utility.py
+++ addons/utility.py
@@ -9,38 +9,37 @@ class Utility:
         self.bot = bot
         print('Addon "{}" loaded'.format(self.__class__.__name__))
         
-        
+    @commands.has_permissions(ban_members=True)
     @commands.command()
     async def reload(self, ctx):
         """Reloads an addon."""
-        if ctx.author == ctx.guild.owner or ctx.author.name == "bernardogiordano":
-            errors = ""
-            for addon in os.listdir("addons"):
-                if ".py" in addon:
-                    addon = addon.replace('.py', '')
-                    try:
-                        self.bot.unload_extension("addons.{}".format(addon))
-                        self.bot.load_extension("addons.{}".format(addon))
-                    except Exception as e:
-                        errors += 'Failed to load addon: `{}.py` due to `{}: {}`\n'.format(addon, type(e).__name__, e)
-            if not errors:
-                await ctx.send(':white_check_mark: Extensions reloaded.')
-            else:
-                await ctx.send(errors)
+        errors = ""
+        for addon in os.listdir("addons"):
+            if ".py" in addon:
+                addon = addon.replace('.py', '')
+                try:
+                    self.bot.unload_extension("addons.{}".format(addon))
+                    self.bot.load_extension("addons.{}".format(addon))
+                except Exception as e:
+                    errors += 'Failed to load addon: `{}.py` due to `{}: {}`\n'.format(addon, type(e).__name__, e)
+        if not errors:
+            await ctx.send(':white_check_mark: Extensions reloaded.')
         else:
-            await ctx.send("You don't have permission to do that!")
+            await ctx.send(errors)
+    else:
+        await ctx.send("You don't have permission to do that!")
             
+    @commands.has_permissions(ban_members=True) 
     @commands.command()
     async def restart(self, ctx):
         """Restarts the bot, obviously"""
-        if ctx.author == ctx.guild.owner or ctx.author.name == "bernardogiordano":
-            await ctx.send("Restarting...")
-            with open("restart.txt", "w+") as f:
-                f.write(str(ctx.message.channel.id))
-                f.close()
-            sys.exit(0)
-        else:
-            await ctx.send("You don't have permission to do that!")
+        await ctx.send("Restarting...")
+        with open("restart.txt", "w+") as f:
+            f.write(str(ctx.message.channel.id))
+            f.close()
+        sys.exit(0)
+    else:
+        await ctx.send("You don't have permission to do that!")
             
 def setup(bot):
     bot.add_cog(Utility(bot))
