fdf933fe759d33232c1fb38c6d5227dded300484
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-11-12 00:46:36 -0800

    reee

--- update.bat
+++ update.bat
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-cd /d <selfbot folder path>
-git add . 
-git commit -am "Update Heroku"
-git push heroku master -f
-=======
 cd /d C:\Users\gpgro\GitHub\PKSMBot
 git add . 
 git commit -am "Update Heroku"
-git push heroku master -f
->>>>>>> be5e151f2fcbcfbd6488990e50900541cb383f1f
+git push heroku master
