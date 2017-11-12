bc5771596f62e10ccf02442a79d493e6e00c1948
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-11-12 01:12:01 -0800

    Change about to no embed.

--- addons/info.py
+++ addons/info.py
@@ -15,9 +15,7 @@ class Info:
         
     @commands.command()
     async def about(self, ctx):
-        embed = discord.Embed(description="This is a bot coded in python for use in the PKSM server. You can view the source code [here](https://github.com/GriffinG1/PSKMBot).")
-        embed.set_author(name="Griffin#2329", icon_url=self.bot.guild.get_member(177939404243992578).avatar_url_as(format="gif"))
-        await ctx.send(embed=embed)
+        await ctx.send("This is a bot coded in python for use in the PKSM server, made by Griffin#2329. You can view the source code [here](https://github.com/GriffinG1/PSKMBot).")
         
 def setup(bot):
     bot.add_cog(Info(bot))
