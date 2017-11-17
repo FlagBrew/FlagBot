0eefea0102fcfd1109822943fa6ad0f1868a0c6b
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-11-16 23:49:21 -0800

    patreon command

--- addons/info.py
+++ addons/info.py
@@ -22,12 +22,17 @@ class Info:
     @commands.command()
     async def about(self, ctx):
         """Information about the bot"""
-        await ctx.send("This is a bot coded in python for use in the PKSM server, made by Griffin#2329. You can view the source code here: https://github.com/GriffinG1/PKSMBot.")
+        await ctx.send("This is a bot coded in python for use in the PKSM server, made by Griffin#2329. You can view the source code here: <https://github.com/GriffinG1/PKSMBot>.")
     
     @commands.command()
     async def why(self, ctx):
         """Why the wait?"""
         await ctx.send("The wait is for two reasons.\n**1.** Bernardo wants to enjoy the game first. Sun and Moon wasn't enjoyable because PKSM was updated right away.\n**2.** People keep asking, when it'll be released, refusing to let Bernardo have a break.")
+        
+    @commands.command()
+    async def patreon(self, ctx):
+        """Donate here"""
+        await ctx.send("You can donate to Bernardo on Patreon here: <https://www.patreon.com/bernardogiordano>.")
         
 def setup(bot):
     bot.add_cog(Info(bot))
