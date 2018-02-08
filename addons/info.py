63196be3fcbc259d668474d638e202ff075c6aba
Griffin <4n0nym0ush4ck3r666@gmail.com>
2018-02-08 02:52:35 -0800

    Remove  from pksm-tools

--- addons/info.py
+++ addons/info.py
@@ -8,7 +8,7 @@ desc = "You can get the latest release of {}."
 desc_pksm = "PKSM [here](https://github.com/BernardoGiordano/PKSM/releases/latest)"
 desc_checkpoint = "Checkpoint [here](https://github.com/BernardoGiordano/Checkpoint/releases/latest)"
 desc_pickr = "Pickr3DS [here](https://github.com/BernardoGiordano/Pickr3DS/releases/latest)"
-desc_tools = "PKSM-Tools [here](https://github.com/BernardoGiordano/PKSM-Tools/releases/latest)"
+desc_tools = "PKSM-Tools [here](https://github.com/BernardoGiordano/PKSM-Tools/releases)"
 desc_lasagna = "Lasagna [here](https://github.com/BernardoGiordano/Lasagna/releases/latest)"
 
 class Info:
