24de92c0266c7166a05d08d30661bf374f8cc61b
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-01-27 20:47:58 -0700

    :/

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ token = config.token
 bot = commands.Bot(command_prefix=prefix, description=description)
 
 bot.is_mongodb = config.is_mongodb
-if not config.localhost_port == 0:
+if not config.localhost_port == "":
     bot.api_url = "http://localhost:" + config.localhost_port + "/"
 else:
     bot.api_url = config.api_url
