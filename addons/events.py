0a16077d0ecd2205158c7298c051f687ce799f97
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-07-20 15:14:37 -0600

    ?????

--- addons/events.py
+++ addons/events.py
@@ -217,8 +217,9 @@ class Events(commands.Cog):
                 await self.bot.activity_logs_channel.send(embed=embed_custom)
             except discord.Forbidden:
                 pass
-            except discord.HTTPException:
-                await self.bot.err_logs_channel.send("Failed to log activity for user `{}` (`{}`) with before activity list of `{}` and after activity list of `{}`. Cause?".format(before, before.id, before.activities, after.activities))
+            except Exception as e:
+                err_embed = discord.Embed(description=e)
+                await self.bot.err_logs_channel.send("Failed to log activity for user `{}` (`{}`) with before activity list of `{}` and after activity list of `{}`. Cause?".format(before, before.id, before.activities, after.activities), embed=err_embed)
 
     async def process_reactions(self, reaction):
         positive_votes = 0
