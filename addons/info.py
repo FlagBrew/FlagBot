31ad8b7bce38584f2888352c319f60071fefa3a7
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-11-18 01:37:17 -0700

    Add FAQ entry for gen 3 WCs, add aliases

--- addons/info.py
+++ addons/info.py
@@ -123,6 +123,7 @@ class Info(commands.Cog):
         "swsh",  # pksm faq #2 - switch support
         "emulator",  # pksm faq #3 - emulator cross-use
         "sendpkx",  # pksm faq #7 - sending pkx files
+        "wc3", "gen3events",  # pksm faq #9 - gen 3 events
         "addcode", "fixcheat",  # checkpoint faq #1 - pls add cheat
         "wheregame",  # checkpoint faq #2 - missing games
         "pkcrash",  # checkpoint faq #4 - cheat crash in pkmn games
