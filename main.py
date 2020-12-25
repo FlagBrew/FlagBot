2a01ebbfe7369d3b6fbfdb64feef105c004a36e5
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2020-12-25 16:11:05 -0700

    Add secondary bot channel to list

--- main.py
+++ main.py
@@ -187,6 +187,7 @@ async def on_ready():
                     bot.logs_channel = discord.utils.get(guild.channels, id=351002624721551371)
                     bot.dm_logs_channel = discord.utils.get(guild.channels, id=695681340510699531)
                     bot.bot_channel = discord.utils.get(guild.channels, id=379201279479513100)
+                    bot.bot_channel2 = discord.utils.get(guild.channels, id=658726241288847361)
                     bot.flagbrew_team_role = discord.utils.get(guild.roles, id=758286639784525845)
                     bot.discord_moderator_role = discord.utils.get(guild.roles, id=396988600480301059)
                     bot.patrons_role = discord.utils.get(guild.roles, id=330078911704727552)
