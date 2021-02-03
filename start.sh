c2b3ae918298f3a7ab0d0953c7af99b39b50e2e4
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2021-02-03 11:51:04 -0700

    Try and fix these req issues

--- start.sh
+++ start.sh
@@ -5,5 +5,6 @@ git clone https://github.com/FlagBrew/FlagBot.git
 fi
 cd FlagBot
 git pull
+pip uninstall -r requirements.txt
 pip install --no-cache-dir --upgrade -r requirements.txt
 python main.py
